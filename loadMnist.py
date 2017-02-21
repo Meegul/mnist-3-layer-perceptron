1a2b8c547df2fc0b4e6b98f04ab1a3e166037922
Meegul <micha3lbilstein@gmail.com>
2017-02-21 15:15:50 -0500

    Added trained model for Mnist

--- loadMnist.py
+++ loadMnist.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 label_keys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-def showImage(data, guess, answer):
+def showImage(data, guess, answer, correct):
 	arr = np.array(data, dtype='float')
 	arr = arr.reshape((28, 28))
 	
-	plt.title("Guess={guess}|Answer={answer}".format(guess=guess, answer=answer))
+	plt.title("Guess={guess}|Answer={answer}|Correct={correct}".format(guess=guess, answer=answer, correct=correct))
 	plt.imshow(arr, cmap='gray')
 	plt.show()
 
@@ -31,6 +31,7 @@ n_classes = 10 # 10 possible outputs (0-9 digits)
 x = tf.placeholder("float", [None, n_input])
 y = tf.placeholder("float", [None, n_classes])
 
+# Function to create a 2 layer perceptron using relu activation
 def multilayer_perceptron(x, weights, biases):
 	layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
 	layer_1 = tf.nn.relu(layer_1)
@@ -52,40 +53,35 @@ biases = {
 	'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
+# Create the perceptron
 pred = multilayer_perceptron(x, weights, biases)
 
+# Cost function is avg softmax
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-init = tf.global_variables_initializer()
+# Initialize saver object for restorting the model
+saver = tf.train.Saver()
 
 with tf.Session() as sess:
-	sess.run(init)
+	print("Restoring model...")
+	saver.restore(sess, './saved_models/mnist/model.ckpt')
+	print("Model restored.")
 
-	for epoch in range(training_epochs):
-		avg_cost = 0.
-		total_batch = int(mnist.train.num_examples/batch_size)
-		
-		for i in range(total_batch):
-			batch_x, batch_y = mnist.train.next_batch(batch_size)
-			_, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
-			avg_cost += c/total_batch
-
-		if epoch % display_step == 0:
-			print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-
-	print("Optimization Finished!")
-	
-	print("Beginning evaluation...")
+	print("Beginning evaluation of model...")
 	guesses = tf.argmax(pred, 1).eval({x: mnist.test.images, y: mnist.test.labels})
 	answers = tf.argmax(y, 1).eval({x: mnist.test.images, y: mnist.test.labels})
+	correctness = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1)).eval({x: mnist.test.images, y: mnist.test.labels})
 
-	for i in range(total_batch):
+	# Show all images w/ guess, answer, and correctness
+	for i in range(len(mnist.test.images)):
 		batch_x = mnist.test.images;
 		guess = label_keys[guesses[i]]
 		answer = label_keys[answers[i]]
-		showImage(batch_x[i], guess, answer) 
+		correct = correctness[i]
+		showImage(batch_x[i], guess, answer, correct) 
 
 	correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 	print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
